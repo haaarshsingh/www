8fd54bc83ea7e8ab7d7487ac23cee846afd15441
Harsh Singh <hi.harsh@protonmail.ch>
2022-07-05 17:38:20 +0000

    Upgrade Prisma + use props for image component

--- components/Blog/Image.tsx
+++ components/Blog/Image.tsx
@@ -1,23 +1,33 @@
 import { FC } from 'react'
 import { FiDribbble, FiGithub, FiTwitter } from 'react-icons/fi'
 
-const Image: FC = () => {
+type ImageProps = {
+  title: string
+  description: string
+  readingTime: number
+  words: number
+  date: string
+}
+
+const Image: FC<ImageProps> = ({
+  title,
+  description,
+  readingTime,
+  words,
+  date,
+}) => {
   return (
     <div
       className='w-[1200px] h-[630px] bg-no-repeat flex flex-col justify-between p-12 bg-cover'
       style={{ backgroundImage: `url("/static/BlogBG.jpg")` }}
     >
       <div>
-        <h1 className='text-5xl'>
-          Fresh: The Next-Gen JavaScript Web Framework
-        </h1>
-        <p className='text-2xl mt-4'>
-          An introduction into Deno's 'fresh' new framework
-        </p>
+        <h1 className='text-5xl'>{title}</h1>
+        <p className='text-2xl mt-4'>{description}</p>
       </div>
       <div className='flex justify-between'>
         <p className='text-2xl text-white'>
-          7 minutes • 1364 words • 03 July, 2022
+          {readingTime} minutes • {words} words • {date}
         </p>
         <div className='text-2xl flex items-center'>
           <FiTwitter />
