21ee54f5b3fa0e3b2af9583878176f6134e4b100
Harsh Singh <hi.harsh@protonmail.ch>
2022-07-06 23:01:18 +0000

    Update i18 config for SSR

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,12 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'hn', 'de', 'fr', 'sr'],
     ignoreRoutes: ['/blog/', '/public/'],
+    serializeConfig: false,
+    defaultNS: 'app',
+    serializeConfig: false,
   },
 }
