c181fca4969177f967d9f39079fd094ec5da1080
Harsh Singh <hi.harsh@protonmail.ch>
2022-07-07 19:47:14 +0000

    Removed authentication to test API

--- pages/api/link.ts
+++ pages/api/link.ts
@@ -7,24 +7,15 @@ const link = async (req: NextApiRequest, res: NextApiResponse) => {
   const session: CustomSession | null = await getSession({ req })
   const { slug, url } = req.body
 
-  if (typeof session?.user?.id === 'string') {
-    try {
-      const shortenedLink = prisma.link.create({
-        data: {
-          slug: slug,
-          url: url,
-          userId: session.user.id,
-        },
-      })
+  const shortenedLink = prisma.link.create({
+    data: {
+      slug: slug,
+      url: url,
+      userId: 'cl1pj1tkh00063nv1flxlefw6',
+    },
+  })
 
-      return res.status(200).json({ shortenedLink })
-    } catch (err) {
-      console.error(err)
-      return res.status(509).json({ error: err })
-    }
-  }
-
-  return res.status(401).json({ error: "User isn't authenticated" })
+  return res.status(200).json({ shortenedLink })
 }
 
 export default link
