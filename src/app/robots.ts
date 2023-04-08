22c0d67eae78ac38f35b36ec025196446745b753
Harsh Singh <hi.harsh@protonmail.ch>
2023-04-08 09:31:31 +0000

    auto return robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,8 @@
-const Robots = () => {
+const Robots = () => ({
   rules: [{ userAgent: '*' }],
   sitemap: 'https://harshsingh.xyz/sitemap.xml',
   host: 'https://harshsingh.xyz',
-}
+})
 
 export default Robots
+
