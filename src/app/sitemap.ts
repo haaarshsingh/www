6e989aea0f932718a48315a605d7ebf788272cc9
Harsh Singh <hi.harsh@protonmail.ch>
2023-04-08 03:36:01 -0500

    Added a sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,19 @@
+import { allPosts } from 'contentlayer/generated';
+
+const Sitemap = () => {
+  const posts = allPosts.map((post) => ({
+    url: `https://harshsingh.xyz/writing/${post.slug}`,
+    lastModified: post.published,
+  }));
+
+  const routes = ['', '/about', '/writing'].map(
+    (route) => ({
+      url: `https://harshsingh.xyz${route}`,
+      lastModified: new Date().toISOString().split('T')[0],
+    })
+  );
+
+  return [...routes, ...posts];
+}
+
+export default Sitemap
