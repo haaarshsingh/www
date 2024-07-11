b8aab4638f5651dcd3923c4d8a5bc91f657e623f
Harsh Singh <hi.harsh@protonmail.ch>
2024-07-11 16:25:08 -0500

    fix image url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import Header from "@/components/home/Header";
 import Main from "@/components/home/Main";
 
-export default () => {
-  return (
-    <>
-      <Header />
-      <Main />
-    </>
-  );
-};
+export default () => (
+  <>
+    <div className="h-full w-full bg-[url('https://harshsingh.xyz/og-bg.png')]" />
+    <Header />
+    <Main />
+  </>
+);
