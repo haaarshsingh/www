2f3d807df48b973897d0dbbbc8a100e6f21abea4
Harsh Singh <hi.harsh@protonmail.ch>
2024-07-12 14:53:43 -0500

    add slider duration toggle for tabs

--- src/components/craft/content/ExclusionTabs.tsx
+++ src/components/craft/content/ExclusionTabs.tsx
@@ -4,10 +4,14 @@ import { useState, useEffect, useRef, useMemo } from "react";
 import Post from "../Post";
 import clsx from "clsx";
 import { motion } from "framer-motion";
+import { FiRotateCw } from "react-icons/fi";
 
 type Path = { w: number; offset: number }[];
 
 export default () => {
+  const [reset, setReset] = useState(false);
+  const [duration, setDuration] = useState(0.1);
+
   const [active, setActive] = useState(0);
   const [clipPaths, setClipPaths] = useState<Path>([]);
   const tabRefs = useRef<HTMLButtonElement[]>([]);
@@ -46,6 +50,13 @@ export default () => {
     setClipPaths(newClipPaths);
   }, [tabs]);
 
+  useEffect(() => {
+    if (reset) {
+      setDuration(0.1);
+      setTimeout(() => setReset(false), 200);
+    }
+  }, [reset]);
+
   const getClipPath = (i: number) => {
     if (clipPaths.length === 0) return "";
     const { offset, w } = clipPaths[i];
@@ -57,7 +68,7 @@ export default () => {
       title="Exclusion Tabs"
       description="Tabs that use clipping to blend between inactive and active."
       tags={["react", "tailwindcss", "framer motion"]}
-      className="relative"
+      className="relative overflow-hidden"
     >
       <div
         className={clsx("relative flex w-fit items-center rounded-full p-1")}
@@ -81,7 +92,12 @@ export default () => {
       <motion.div
         style={{ clipPath: getClipPath(active) }}
         animate={{ clipPath: getClipPath(active) }}
-        transition={{ type: "spring", stiffness: 300, damping: 29 }}
+        transition={{
+          type: duration === 0.1 ? "spring" : "tween",
+          stiffness: 300,
+          damping: 29,
+          duration: duration,
+        }}
         className={clsx(
           "[will-change: clip-path] pointer-events-none absolute left-1/2 z-20 flex w-fit -translate-x-1/2 items-center bg-neutral-950 p-1 dark:bg-neutral-50",
         )}
@@ -100,6 +116,28 @@ export default () => {
           </span>
         ))}
       </motion.div>
+      <div className="absolute bottom-0 flex w-full items-center justify-center gap-x-2 border-t border-t-neutral-200 bg-neutral-100 px-8 py-4 dark:border-t-neutral-700/50 dark:bg-neutral-800">
+        <input
+          min={0}
+          max={2}
+          type="range"
+          value={duration}
+          step={0.1}
+          className="h-1 w-full cursor-pointer appearance-none rounded-lg bg-neutral-200 dark:bg-neutral-700"
+          onChange={(e) => setDuration(parseFloat(e.currentTarget.value))}
+        />
+        <motion.button
+          aria-label="reset"
+          className="ml-8"
+          onClick={() => setReset(true)}
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1, rotate: reset ? 180 : 0 }}
+          exit={{ scale: 0, opacity: 0 }}
+          transition={{ type: "spring" }}
+        >
+          <FiRotateCw />
+        </motion.button>
+      </div>
     </Post>
   );
 };
