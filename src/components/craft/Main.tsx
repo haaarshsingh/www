e56261bc34c431889665a231211ba8bc815059af
Harsh Singh <hi.harsh@protonmail.ch>
2024-08-04 19:17:44 -0500

    new interaction

--- src/components/craft/Main.tsx
+++ src/components/craft/Main.tsx
@@ -3,12 +3,14 @@ import ExclusionTabs from "./content/ExclusionTabs";
 import Stepper from "./content/Stepper";
 import ArcTabs from "./content/ArcTabs";
 import RadialSelector from "./content/RadialSelector";
+import Dropdown from "./content/Dropdown";
 
 export default () => (
   <main className="animate-children flex flex-col gap-y-12">
     <DynamicIsland />
-    <RadialSelector />
+    <Dropdown />
     <ExclusionTabs />
+    <RadialSelector />
     <Stepper />
     <ArcTabs />
   </main>
