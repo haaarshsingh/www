45544044a9a4cc12a2d3864876ca8e15152f68be
Harsh Singh <hi.harsh@protonmail.ch>
2024-09-10 12:10:03 -0400

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import vue from "@astrojs/vue";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
   output: "hybrid",
+  adapter: vercel(),
 });
