8a0cc17f96932df9658865d8900b592b8f429440
Harsh Singh <hi.harsh@protonmail.ch>
2024-09-11 15:11:32 -0400

    switch to POST for locate

--- src/pages/api/locate.ts
+++ src/pages/api/locate.ts
@@ -4,19 +4,12 @@ export const prerender = false;
 
 const locate = async (ip: string) => {
   try {
-    const response = await fetch(`https://ipapi.co/${ip}/json/`);
-    const data = await response.json();
+    const location = await fetch(`https://ipapi.co/${ip}/json/`);
 
-    return `${data.city}, ${data.region_code}`;
-  } catch (error) {
-    return console.error("Error fetching IP details:", error);
-  }
-};
+    const data = await location.json();
 
-const setLocation = async (location: string) => {
-  try {
     const response = await fetch(
-      `${import.meta.env.UPSTASH_REDIS_REST_URL}/set/visitor/${encodeURI(location)}`,
+      `${import.meta.env.UPSTASH_REDIS_REST_URL}/set/visitor/${encodeURI(`${data.city}, ${data.region_code}`)}`,
       {
         headers: {
           Authorization: `Bearer ${import.meta.env.UPSTASH_REDIS_REST_TOKEN}`,
@@ -26,13 +19,12 @@ const setLocation = async (location: string) => {
 
     return await response.status;
   } catch (error) {
-    return console.error("Error saving to Upstash:", error);
+    return console.error("Error fetching IP details:", error);
   }
 };
 
-export const GET: APIRoute = async (props) => {
-  const location = await locate(props.clientAddress);
-  const status = await setLocation(location as string);
+export const POST: APIRoute = async (props) => {
+  const status = await locate(props.clientAddress);
 
   return new Response(JSON.stringify({ status: status }));
 };
