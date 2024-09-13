9ad8b19fc0599515cfa0744c6b5f4c596944e5fd
Harsh Singh <hi.harsh@protonmail.ch>
2024-09-13 02:00:38 -0400

    improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://harshsingh.xyz",
+  integrations: [tailwind(), sitemap()],
   output: "hybrid",
   adapter: vercel(),
 });
