ef4953856f7d13968a9c3b979890b00924531bde
Harsh Singh <hi.harsh@protonmail.ch>
2024-11-26 23:46:27 -0600

    add mdx files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://harshsingh.xyz",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), react(), mdx()],
   output: "hybrid",
   adapter: vercel(),
 });
